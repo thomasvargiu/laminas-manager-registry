4a2e386a121bc2c03260fec5fc885769bdf0dace
Thomas Vargiu <tvargiu@gmail.com>
2020-07-06 11:32:39 +0200

    CSFIX

--- tests/functions.php
+++ tests/functions.php
@@ -3,10 +3,11 @@
 namespace TMV\Laminas\ManagerRegistry\Test;
 
 use function class_alias;
+use function trait_exists;
 
 if (! trait_exists(\Prophecy\PhpUnit\ProphecyTrait::class)) {
-    trait ProphecyTrait {
-
+    trait functions
+    {
     }
 
     class_alias(ProphecyTrait::class, \Prophecy\PhpUnit\ProphecyTrait::class);
